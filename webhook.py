93578736b86beb6ece0b67df6c6eecd55ffc0783
Jack Coates <68520860+shexty@users.noreply.github.com>
2025-10-04 18:16:42 +0100

    Update webhook.py
    Descriptions updated for easy how to use

--- webhook.py
+++ webhook.py
@@ -3,14 +3,15 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 from datetime import datetime
 NOW = datetime.now()
 # Please place your webhook here.
-webhook = DiscordWebhook(url='https://discord.com/api/webhooks/812346710760226848/aTSDemQr8-iB7ijZ9Oi0Ceigbom-cdmlYYCMnGbUwnXzbl5wOGOCfTjBNA2F364_CM2x', username="Insecurity Network")
-embed = DiscordEmbed(title='DDoS Alert (PPS Threshold Reached)', url='https://cybersniff.net', description='Insecuritys France OVH is underattack.', color=3447003)
+webhook = DiscordWebhook(url='DISCORD WEBHOOK', username="USERNAME OF BOT")
+embed = DiscordEmbed(title='DDoS Alert (PPS Threshold Reached)', url='https://cybersniff.net', description='DESCRIPTION OF MESSAGE', color=3447003)
 embed.set_footer(text='Attack dump stored in: /home/ubuntu/dumps/')
 embed.set_timestamp()
-embed.add_embed_field(name='**Protection Provider:**', value='Shextys Firewall')
-embed.add_embed_field(name='**Location:**', value='	Paris, France')
+embed.add_embed_field(name='**Protection Provider:**', value='NAME OF PROIVDER')
+embed.add_embed_field(name='**Location:**', value='LOCATION OF VPN')
 embed.add_embed_field(name='**IP Address:**', value='151.80.217.215')
 embed.set_thumbnail(url='https://flaglane.com/download/french-flag/french-flag-medium.jpg')
 embed.set_footer(text='Automatic Packet Dump has been initiated!')
 webhook.add_embed(embed)
 response = webhook.execute()
+
